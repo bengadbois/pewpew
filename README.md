68837a2759509bba793e258124bf7d8c0b128f43
Ben Gadbois <git@bengadbois.email>
2016-10-13 20:55:02 +0200

    Basic README, dependencies, basic setup for stress command

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #Pewpew
 
-Load tester written in Go
+HTTP(S) 1.1 and HTTP2 stress tester
+
+#Disclaimer
+Pewpew is designed as a tool to help those developing web services and websites. Please use responsibly.
+
+#Building
+Requires Golang 1.7+ and Glide
+```glide install```
+
---
+++ cmd/root.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "pewpew",
+	Short: "HTTP(S) & HTTP2 load tester",
+	Long: `HTTP(S) & HTTP2 load tester long description TODO fill out more
+	`, //TODO
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pewpew.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".pewpew") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+	viper.AutomaticEnv()           // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
