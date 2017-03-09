7bd1eb1171a51a6bca51a226e7192ae08c39dc1b
Ben Gadbois <git@bengadbois.email>
2017-03-09 20:16:38 +0100

    Refactoring request stats and printing while running
    This also includes a segfault fix for counting data transferred on a failed request

--- lib/printer.go
+++ lib/printer.go
@@ -2,7 +2,11 @@ package pewpew
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"sort"
+
+	color "github.com/fatih/color"
 )
 
 //creates nice readable summary of entire stress test
@@ -38,4 +42,53 @@ func createTextSummary(reqStatSummary requestStatSummary) string {
 		summary = summary + "Failed: " + fmt.Sprintf("%d", reqStatSummary.statusCodes[0]) + " requests\n"
 	}
 	return summary
+}
+
+//print colored single line stats per requestStat
+func printStat(stat requestStat) {
+	if stat.Error != nil {
+		color.Set(color.FgRed)
+		fmt.Println("Failed to make request: " + stat.Error.Error())
+		color.Unset()
+	} else {
+		if stat.StatusCode >= 100 && stat.StatusCode < 200 {
+			color.Set(color.FgBlue)
+		} else if stat.StatusCode >= 200 && stat.StatusCode < 300 {
+			color.Set(color.FgGreen)
+		} else if stat.StatusCode >= 300 && stat.StatusCode < 400 {
+			color.Set(color.FgCyan)
+		} else if stat.StatusCode >= 400 && stat.StatusCode < 500 {
+			color.Set(color.FgMagenta)
+		} else {
+			color.Set(color.FgRed)
+		}
+		fmt.Printf("%s %d\t%d bytes\t%d ms\t-> %s %s\n",
+			stat.Proto,
+			stat.StatusCode,
+			stat.DataTransferred,
+			stat.Duration.Nanoseconds()/1000000,
+			stat.Method,
+			stat.URL)
+		color.Unset()
+	}
+}
+
+//print tons of info about the request, response and response body
+func printVerbose(req *http.Request, response *http.Response) {
+	var requestInfo string
+	//request details
+	requestInfo = requestInfo + fmt.Sprintf("Request:\n%+v\n\n", &req)
+
+	//reponse metadata
+	requestInfo = requestInfo + fmt.Sprintf("Response:\n%+v\n\n", response)
+
+	//reponse body
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		requestInfo = requestInfo + fmt.Sprintf("Failed to read response body: %s\n", err.Error())
+	} else {
+		requestInfo = requestInfo + fmt.Sprintf("Body:\n%s\n\n", body)
+		response.Body.Close()
+	}
+	fmt.Println(requestInfo)
 }
---
+++ lib/requester.go
@@ -0,0 +1,48 @@
+package pewpew
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+func runRequest(req http.Request, client *http.Client) (response *http.Response, stat requestStat) {
+	reqStartTime := time.Now()
+	response, responseErr := (*client).Do(&req)
+	reqEndTime := time.Now()
+
+	if responseErr != nil {
+		stat = requestStat{
+			Proto:           req.Proto,
+			URL:             req.URL.String(),
+			Method:          req.Method,
+			StartTime:       reqStartTime,
+			EndTime:         reqEndTime,
+			Duration:        reqEndTime.Sub(reqStartTime),
+			StatusCode:      0,
+			Error:           responseErr,
+			DataTransferred: 0,
+		}
+		return
+	}
+
+	//get size of request
+	reqDump, _ := httputil.DumpRequestOut(&req, true)
+	respDump, _ := httputil.DumpResponse(response, true)
+	totalSizeSentBytes := len(reqDump)
+	totalSizeReceivedBytes := len(respDump)
+	totalSizeBytes := totalSizeSentBytes + totalSizeReceivedBytes
+
+	stat = requestStat{
+		Proto:           req.Proto,
+		URL:             req.URL.String(),
+		Method:          req.Method,
+		StartTime:       reqStartTime,
+		EndTime:         reqEndTime,
+		Duration:        reqEndTime.Sub(reqStartTime),
+		StatusCode:      response.StatusCode,
+		Error:           responseErr,
+		DataTransferred: totalSizeBytes,
+	}
+	return
+}
